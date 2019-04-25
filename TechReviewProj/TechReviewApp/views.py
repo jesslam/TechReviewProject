f466cbfd41300937ce0d56650f321f785790904f
jesslam <49288633+jesslam@users.noreply.github.com>
2019-04-25 15:09:27 -0700

    Add files via upload

--- TechReviewProj/TechReviewApp/views.py
+++ TechReviewProj/TechReviewApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import TechType, Product, Review
 
 # Create your views here.
+def index(request):
+    return render(request, 'TechReviewApp/index.html')
+
+def getTypes(request):
+    types_list=TechType.objects.all()
+    context={'types_list' : types_list}
+    return render(request, 'TechReviewApp/types.html', context=context)
