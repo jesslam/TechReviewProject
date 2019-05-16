6f9d516d1530a44278f8dfb32181ef1afb108139
jesslam <49288633+jesslam@users.noreply.github.com>
2019-05-16 11:02:15 -0700

    Delete admin.py

--- TechReviewProj/TechReviewApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TechType, Product, Review
-
-# Register your models here.
-admin.site.register(TechType)
-admin.site.register(Product)
-admin.site.register(Review)
